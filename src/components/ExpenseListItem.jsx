da3b2e4208d3963951e543709fdf62a583396e84
Prince-Turbo <shridharpai.m@gmail.com>
2021-07-28 23:00:27 +0530

    Currency formating and date formatting final

--- src/components/ExpenseListItem.jsx
+++ src/components/ExpenseListItem.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import moment from 'moment';
+import numeral from 'numeral';
+
 
 const ExpenseListItem = ({description, id, createdAt, amount}) => (
     <div>
         <Link to={`/edit/${id}`} ><h4>{description}</h4></Link>
-        <p>{amount} - {moment(createdAt).format("MMM Do, YYYY")}</p>
+        <p>{"₹" + numeral(amount).format('0,0.00')} - {moment(createdAt).format("MMM Do, YYYY")}</p>
         
     </div>
 );
