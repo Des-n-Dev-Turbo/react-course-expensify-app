d58f48c4db61249db98592b76c5fb58f30afe4f8
Prince-Turbo <shridharpai.m@gmail.com>
2021-07-28 17:46:41 +0530

    Production Setup 2nd Stage

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = (env) => {
-    console.log("env", env);
+  const CSSExtract = new MiniCssExtractPlugin({ filename : "styles.css" });
   return {
     entry: "./src/app.js",
     output: {
@@ -17,15 +18,32 @@ module.exports = (env) => {
         },
         {
           test: /\.s?css$/,
-          use: ["style-loader", "css-loader", "sass-loader"],
+          use: [
+            MiniCssExtractPlugin.loader, 
+            {
+              loader: "css-loader",
+              options: {
+                sourceMap: true
+              }
+            }, 
+            {
+              loader: "sass-loader",
+              options: {
+                sourceMap: true
+              }
+            }
+          ],
         },
       ],
     },
+    plugins: [
+      CSSExtract
+    ],
     resolve: {
       extensions: [".js", ".jsx"],
     },
     mode: env.production ? "production" : "development",
-    devtool: env.production ? "source-map" : "eval-cheap-module-source-map",
+    devtool: env.production ? "source-map" : "inline-source-map",
     devServer: {
       contentBase: path.join(__dirname, "public"),
       historyApiFallback: true,
@@ -33,32 +51,3 @@ module.exports = (env) => {
   };
 };
 
-// module.exports = {
-//   entry: "./src/app.js",
-//   output: {
-//     path: path.join(__dirname, "public"),
-//     filename: "bundle.js",
-//   },
-//   module: {
-//     rules: [
-//       {
-//         loader: "babel-loader",
-//         test: /\.jsx?$/,
-//         exclude: /node_modules$/,
-//       },
-//       {
-//         test: /\.s?css$/,
-//         use: ["style-loader", "css-loader", "sass-loader"],
-//       },
-//     ],
-//   },
-//   resolve: {
-//     extensions: [".js", ".jsx"],
-//   },
-//   mode: "development",
-//   devtool: "eval-cheap-module-source-map",
-//   devServer: {
-//     contentBase: path.join(__dirname, "public"),
-//     historyApiFallback: true,
-//   },
-// };
